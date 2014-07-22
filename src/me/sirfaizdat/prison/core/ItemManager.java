67c5148fad65ee129916d7b879ee32a43f0889e8
SirFaizdat <sirfaizdat@gmail.com>
2014-07-22 04:09:36 -0400

    Working towards 2.0.5

--- src/me/sirfaizdat/prison/core/ItemManager.java
+++ src/me/sirfaizdat/prison/core/ItemManager.java
@@ -1,5 +1,18 @@
 /**
- * (C) 2014 SirFaizdat
+  	Copyright (C) 2014 SirFaizdat
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package me.sirfaizdat.prison.core;
 
@@ -69,8 +82,8 @@ public class ItemManager {
 
 			in.close();
 		} catch (Exception e) {
-			Core.l.warning("Could not read item list! Attempting to use local items.csv...");
-			Core.l.info("While this lets you use item names, the local list is not up-to-date with the latest Minecraft blocks.");
+			Core.l.warning("Could not read item list from internet! Attempting to use local items.csv...");
+			Core.l.info("While this lets you use item names, the local list is not as up-to-date with the latest Minecraft blocks as the online version is.");
 
 			try {
 				BufferedReader in = new BufferedReader(new FileReader(backup));
