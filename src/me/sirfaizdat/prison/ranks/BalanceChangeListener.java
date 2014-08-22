b20734e5e18bf7eee16fad1b1608871636b836bb
SirFaizdat <sirfaizdat@gmail.com>
2014-08-21 20:46:53 -0400

    Failed attempt at adding a new configuration system - hope I didn't break anything.

--- src/me/sirfaizdat/prison/ranks/BalanceChangeListener.java
+++ src/me/sirfaizdat/prison/ranks/BalanceChangeListener.java
@@ -29,37 +29,28 @@ class BalanceChangeListener implements Listener {
 			balance.put(p.getName(), eco.getBalance(p));
 		}
 		Bukkit.getServer().getPluginManager().registerEvents(this, Prison.i());
-		Bukkit.getScheduler().scheduleSyncRepeatingTask(Prison.i(),
-				new Runnable() {
-					@Override
-					public void run() {
-						for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-							if (eco.getBalance(p) != balance.get(p.getName())) {
-								Bukkit.getServer()
-										.getPluginManager()
-										.callEvent(
-												new BalanceChangeEvent(
-														p,
-														balance.get(p.getName()),
-														eco.getBalance(p)));
-								balance.remove(p.getName());
-								balance.put(p.getName(), eco.getBalance(p));
-							}
-						}
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(Prison.i(), new Runnable() {
+			@Override
+			public void run() {
+				for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+					if (eco.getBalance(p) != balance.get(p.getName())) {
+						Bukkit.getServer().getPluginManager().callEvent(new BalanceChangeEvent(p, balance.get(p.getName()), eco.getBalance(p)));
+						balance.remove(p.getName());
+						balance.put(p.getName(), eco.getBalance(p));
 					}
-				}, 5 * 20L, 5 * 20L);
+				}
+			}
+		}, 5 * 20L, 5 * 20L);
 	}
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		balance.put(event.getPlayer().getName(),
-				eco.getBalance(event.getPlayer()));
+		balance.put(event.getPlayer().getName(), eco.getBalance(event.getPlayer()));
 	}
 
 	@EventHandler
 	public void onPlayerLeave(PlayerQuitEvent event) {
-		if (balance.containsKey(event.getPlayer().getName()))
-			balance.remove(event.getPlayer().getName());
+		if (balance.containsKey(event.getPlayer().getName())) balance.remove(event.getPlayer().getName());
 	}
 
 }
