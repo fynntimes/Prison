e77288d7e247aff3e81fee314404038ec70b834a
SirFaizdat <sirfaizdat@gmail.com>
2015-11-16 16:06:53 -0500

    This is v2.1.5_r1

--- src/me/sirfaizdat/prison/core/Config.java
+++ src/me/sirfaizdat/prison/core/Config.java
@@ -12,48 +12,52 @@ import org.bukkit.configuration.file.FileConfiguration;
  */
 public class Config {
 
-	public String serverPrefix;
-	public boolean checkUpdates;
+    public String serverPrefix;
+    public boolean checkUpdates;
 
-	public int resetTime;
-	public List<Integer> resetWarnings;
-	public boolean fillMode;
-	public String resetWarningMessage;
-	public String resetBroadcastMessage;
-	
-	public List<String> rankWorlds;
-	public boolean enableMultiworld;
+    public int resetTime;
+    public List<Integer> resetWarnings;
+    public boolean fillMode;
+    public String resetWarningMessage;
+    public String resetBroadcastMessage;
 
-	public Config() {
-		FileConfiguration c = Prison.i().getConfig();
+    public List<String> rankWorlds;
+    public boolean enableMultiworld;
 
-		serverPrefix = Prison.colorize(c.getString("server-prefix") + " &r");
-		checkUpdates = c.getBoolean("check-updates");
-		resetTime = c.getInt("reset-time");
-		resetWarnings = c.getIntegerList("reset-warnings");
-		fillMode = c.getBoolean("fill-mode");
-		resetWarningMessage = Prison.colorize(c
-				.getString("reset-warning-message"));
-		resetBroadcastMessage = Prison.colorize(c
-				.getString("reset-broadcast-message"));
-		rankWorlds = c.getStringList("world-list");
-		enableMultiworld = c.getBoolean("multiworld");
-	}
+    public Config() {
+        FileConfiguration c = Prison.i().getConfig();
 
-	public void reload() {
-		Prison.i().reloadConfig();
-		FileConfiguration c = Prison.i().getConfig();
+        try {
+            serverPrefix = Prison.colorize(c.getString("server-prefix") + " &r");
+            checkUpdates = c.getBoolean("check-updates");
+            resetTime = c.getInt("reset-time");
+            resetWarnings = c.getIntegerList("reset-warnings");
+            fillMode = c.getBoolean("fill-mode");
+            resetWarningMessage = Prison.colorize(c
+                    .getString("reset-warning-message"));
+            resetBroadcastMessage = Prison.colorize(c
+                    .getString("reset-broadcast-message"));
+            rankWorlds = c.getStringList("world-list");
+            enableMultiworld = c.getBoolean("multiworld");
+        } catch (NullPointerException e) {
+            Prison.l.severe("Your configuration is missing a setting or two. Try deleting it and reloading the server.");
+        }
+    }
 
-		serverPrefix = Prison.colorize(c.getString("server-prefix") + " &r");
-		checkUpdates = c.getBoolean("check-updates");
-		resetTime = c.getInt("reset-time");
-		resetWarnings = c.getIntegerList("reset-warnings");
-		fillMode = c.getBoolean("fill-mode");
-		resetWarningMessage = Prison.colorize(c
-				.getString("reset-warning-message"));
-		resetBroadcastMessage = Prison.colorize(c
-				.getString("reset-broadcast-message"));
-		enableMultiworld = c.getBoolean("multiworld");
-	}
+    public void reload() {
+        Prison.i().reloadConfig();
+        FileConfiguration c = Prison.i().getConfig();
+
+        serverPrefix = Prison.colorize(c.getString("server-prefix") + " &r");
+        checkUpdates = c.getBoolean("check-updates");
+        resetTime = c.getInt("reset-time");
+        resetWarnings = c.getIntegerList("reset-warnings");
+        fillMode = c.getBoolean("fill-mode");
+        resetWarningMessage = Prison.colorize(c
+                .getString("reset-warning-message"));
+        resetBroadcastMessage = Prison.colorize(c
+                .getString("reset-broadcast-message"));
+        enableMultiworld = c.getBoolean("multiworld");
+    }
 
 }
