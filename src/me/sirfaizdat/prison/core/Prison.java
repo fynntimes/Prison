06860e4c41e22bda776ed8e87ec2bed311eb8df5
Camouflage100 <cameowolfe100@gmail.com>
2016-06-19 18:30:40 -0500

    Added a warning for server owners that are currently using Java 7 or lower that Prison-3 will only be compatible with Java 8.

--- src/me/sirfaizdat/prison/core/Prison.java
+++ src/me/sirfaizdat/prison/core/Prison.java
@@ -76,6 +76,7 @@ public class Prison extends JavaPlugin implements Listener {
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
 
+        verifyJavaVersion();
         bootstrap();
         initComponents();
         initCommands();
@@ -230,6 +231,14 @@ public class Prison extends JavaPlugin implements Listener {
         if (!hasPlugin("WorldEdit")) {
             mines.setEnabled(false);
             l.warning("Could not enable Mines because WorldEdit is not loaded.");
+        }
+    }
+
+    private void verifyJavaVersion() {
+        String version = System.getProperty("java.version");
+
+        if (version.charAt(2) <= '7') {
+            l.warning("Prison-3 will only be compatible with Java 8. Please update your Java version, or find a better shared hosting company. ");
         }
     }
 
