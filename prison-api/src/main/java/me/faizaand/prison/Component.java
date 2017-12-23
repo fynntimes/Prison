773e28d53d2655a11ed82ee6000e9fbc548b632c
Faizaan Datoo <fdatoo7@gmail.com>
2017-12-22 23:34:26 -0500

    Separate API from the rest

---
+++ prison-api/src/main/java/me/faizaand/prison/Component.java
@@ -0,0 +1,9 @@
+package me.faizaand.prison;
+
+/**
+ * A component performs one task in the plugin.
+ * They are essentially independent of each other, aside from the APIs that they choose to expose.
+ * They provide modularity to the system so that users can toggle features at will.
+ */
+public interface Component {
+}
---
+++ prison-common/src/main/java/me/faizaand/prison/Prison.java
@@ -0,0 +1,8 @@
+package me.faizaand.prison;
+
+/**
+ * Bootstraps API systems and components.
+ * This is the main entry-point for the plugin's logic, aside from implementations.
+ */
+public class Prison {
+}
