a443a1a116c0fae65bdc957d792bf8f39453fdc7
Faizaan Datoo <fdatoo7@gmail.com>
2017-12-22 19:07:53 -0500

    Let's get this project started!

---
+++ prison-spigot/src/main/java/me/faizaand/prison/PrisonSpigot.java
@@ -0,0 +1,17 @@
+package me.faizaand.prison;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrisonSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
